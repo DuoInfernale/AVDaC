ff7c7b2fdc6a2278589c0f26624f3b6538d5bb6c
Flavio Meyer <flavio.meyer@flavio-meyer.ch>
2026-06-01 15:12:09 +0200

    Delete Customer examples

--- Customers/ACME/providers.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "temp-state"
-    storage_account_name = "avdtofustate"
-    container_name       = "avd"
-    key                  = "acme_avd.tfstate"
-    use_azuread_auth     = true
-  }
-}
